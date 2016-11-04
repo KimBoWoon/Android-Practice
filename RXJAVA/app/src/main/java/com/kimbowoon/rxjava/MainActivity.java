03289d949609b483cfe4356d8ac87c9d5bb7ae98
KimBoWoon <haioer@naver.com>
2016-11-05 00:08:35 +0900

    retrolambda

--- RXJAVA/app/src/main/java/com/kimbowoon/rxjava/MainActivity.java
+++ RXJAVA/app/src/main/java/com/kimbowoon/rxjava/MainActivity.java
@@ -1,43 +1,36 @@
 package com.kimbowoon.rxjava;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
 
-import rx.Observable;
-import rx.Subscriber;
-import rx.Subscription;
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import rx.Observable;
+
+// RxAndroid는 메모리 누수가 있어서 ThirdParty Library를 사용해 메모리를 관리
+
+/**
+ * RxAppCompatActivity는 RxLifeCycle에 있는 클래스
+ * onCreate()에서 Subscribe하면 onDestroy()에서 Unsubscribe
+ * onResume()에서 Subscribe하면 onPause()에서 Unsubscribe
+ * 만약 Unsubscribe 시점을 바꾸고 싶다면 bindUntilEvent로 조정할 수 있음
+ */
+public class MainActivity extends RxAppCompatActivity {
     private final String TAG = MainActivity.class.getName();
+
+    @Bind(R.id.textView)
+    TextView simpleTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable.just("Hello RxAndroid!!")
+                .compose(this.<String>bindToLifecycle())
+                .subscribe(s -> {
+                    simpleTextView.setText(s);
+                });
     }
-
-    Subscription simpleObservable = Observable.create(new Observable.OnSubscribe<String>() {
-        @Override
-        public void call(Subscriber<? super String> subscriber) {
-            subscriber.onNext("Hello RxAndroid !!");
-            subscriber.onCompleted();
-        }
-    }).subscribe(new Subscriber<String>() {
-        @Override
-        public void onCompleted() {
-            Log.d(TAG, "complete!");
-        }
-
-        @Override
-        public void onError(Throwable e) {
-            Log.e(TAG, "error: " + e.getMessage());
-        }
-
-        @Override
-        public void onNext(String text) {
-            ((TextView) findViewById(R.id.textView)).setText(text);
-        }
-    });
 }
