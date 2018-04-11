d31c1c9b4f8bdc7a25976c6e2c056c1287818645
KimBoWoon <haioer@naver.com>
2018-04-12 00:13:24 +0900

    create thumbnail image

--- AndroidVideoView/app/src/main/java/com/bowoon/android/android_videoview/RecyclerAdapter.java
+++ AndroidVideoView/app/src/main/java/com/bowoon/android/android_videoview/RecyclerAdapter.java
@@ -2,14 +2,16 @@ package com.bowoon.android.android_videoview;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.provider.MediaStore;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.android.logcat.log.ALog;
 
 import java.util.List;
 
@@ -57,6 +59,8 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
         }
 
         ((ItemViewHolder) holder).getTitle().setText(items.get(position).getTitle());
+        Bitmap thumbnail = ThumbnailUtils.createVideoThumbnail(items.get(position).getPath(), MediaStore.Images.Thumbnails.MINI_KIND);
+        ((ItemViewHolder) holder).getThumbnail().setImageBitmap(thumbnail);
     }
 
     @Override
@@ -66,10 +70,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     static class ItemViewHolder extends RecyclerView.ViewHolder {
         private TextView title;
+        private ImageView thumbnail;
 
         public ItemViewHolder(View itemView, final ItemClickListener itemClickListener) {
             super(itemView);
+
             this.title = (TextView) itemView.findViewById(R.id.video_title);
+            this.thumbnail = (ImageView) itemView.findViewById(R.id.video_thumbnail);
+
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -84,6 +92,14 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
         public void setTitle(TextView title) {
             this.title = title;
+        }
+
+        public ImageView getThumbnail() {
+            return thumbnail;
+        }
+
+        public void setThumbnail(ImageView thumbnail) {
+            this.thumbnail = thumbnail;
         }
     }
 }
