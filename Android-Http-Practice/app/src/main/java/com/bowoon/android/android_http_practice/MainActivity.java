01f9ca117cf59727eb2e66b51bf20ca30d3f9e0b
KimBoWoon <haioer@naver.com>
2018-12-05 14:59:39 +0900

    update okhttp

--- Android-Http-Practice/app/src/main/java/com/bowoon/android/android_http_practice/MainActivity.java
+++ Android-Http-Practice/app/src/main/java/com/bowoon/android/android_http_practice/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 
 import com.bowoon.android.android_http_practice.common.HttpCallback;
 import com.bowoon.android.android_http_practice.common.HttpService;
@@ -42,19 +43,40 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-        HttpService volley = HttpServiceFactory.createClass("volley", getApplicationContext());
-        volley.getPerson(new HttpCallback() {
+//        HttpService volley = HttpServiceFactory.createClass("volley", getApplicationContext());
+//        volley.getPerson(new HttpCallback() {
+//            @Override
+//            public void onSuccess(Object o) {
+//                if (o instanceof Person) {
+//                    RecyclerAdapter adapter = new RecyclerAdapter(getApplicationContext(), ((Person) o).getItems());
+//                    recyclerView.setAdapter(adapter);
+//                }
+//            }
+//
+//            @Override
+//            public void onFail() {
+//
+//            }
+//        });
+
+        HttpService okHttp = HttpServiceFactory.createClass("okhttp");
+        okHttp.getPerson(new HttpCallback() {
             @Override
             public void onSuccess(Object o) {
                 if (o instanceof Person) {
-                    RecyclerAdapter adapter = new RecyclerAdapter(getApplicationContext(), ((Person) o).getItems());
-                    recyclerView.setAdapter(adapter);
+                    final RecyclerAdapter adapter = new RecyclerAdapter(getApplicationContext(), ((Person) o).getItems());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            recyclerView.setAdapter(adapter);
+                        }
+                    });
                 }
             }
 
             @Override
             public void onFail() {
-
+                Log.d("error", "OkHttp");
             }
         });
     }
--- Android-Http-Practice/app/src/main/java/com/bowoon/android/android_http_practice/common/HttpServiceFactory.java
+++ Android-Http-Practice/app/src/main/java/com/bowoon/android/android_http_practice/common/HttpServiceFactory.java
@@ -2,19 +2,28 @@ package com.bowoon.android.android_http_practice.common;
 
 import android.content.Context;
 
+import com.bowoon.android.android_http_practice.okhttp.UseOkHttp;
 import com.bowoon.android.android_http_practice.retrofit.UseRetrofit;
 import com.bowoon.android.android_http_practice.volley.UseVolley;
 
 public class HttpServiceFactory {
-    public static HttpService createClass(String name) throws IllegalAccessException {
-        switch (name) {
-            case "retrofit":
-                return new UseRetrofit();
-            case "volley":
-                return new UseVolley();
-            default:
-                return null;
+    public static HttpService createClass(String name) {
+        try {
+            switch (name) {
+                case "retrofit":
+                    return new UseRetrofit();
+                case "volley":
+                    return new UseVolley();
+                case "okhttp":
+                    return new UseOkHttp();
+                default:
+                    return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        return null;
     }
 
     public static HttpService createClass(String name, Context context) {
@@ -23,6 +32,8 @@ public class HttpServiceFactory {
                 return new UseRetrofit();
             case "volley":
                 return new UseVolley(context);
+            case "okhttp":
+                return new UseOkHttp();
             default:
                 return null;
         }
