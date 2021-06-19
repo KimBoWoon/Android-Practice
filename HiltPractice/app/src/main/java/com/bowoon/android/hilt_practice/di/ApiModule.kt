a91fca3645677645a40a16d829ca1d52146a97da
KimBoWoon <haioer@naver.com>
2021-06-19 19:56:59 +0900

    di module 분리

---
+++ HiltPractice/app/src/main/java/com/bowoon/android/hilt_practice/di/ApiModule.kt
@@ -0,0 +1,21 @@
+package com.bowoon.android.hilt_practice.di
+
+import com.bowoon.android.hilt_practice.api.ApiHelperImpl
+import com.bowoon.android.hilt_practice.api.providePersonApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApiModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit() = providePersonApi()
+
+    @Provides
+    @Singleton
+    fun providePersonApiImpl() = ApiHelperImpl(provideRetrofit())
+}
--- HiltPractice/app/src/main/java/com/bowoon/android/hilt_practice/di/GlobalModule.kt
+++ HiltPractice/app/src/main/java/com/bowoon/android/hilt_practice/di/GlobalModule.kt
@@ -1,7 +1,5 @@
 package com.bowoon.android.hilt_practice.di
 
-import com.bowoon.android.hilt_practice.api.ApiHelperImpl
-import com.bowoon.android.hilt_practice.api.providePersonApi
 import com.google.gson.Gson
 import dagger.Module
 import dagger.Provides
@@ -15,12 +13,4 @@ class GlobalModule {
     @Provides
     @Singleton
     fun provideGson() = Gson()
-
-    @Provides
-    @Singleton
-    fun provideRetrofit() = providePersonApi()
-
-    @Provides
-    @Singleton
-    fun providePersonApiImpl() = ApiHelperImpl(provideRetrofit())
 }
